336c177334753e8235d3594f450ed25df410c2f5
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2024-01-03 23:04:07 +0900

    temp

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,33 @@
-use std::ops::Deref;
+use std::{
+    ops::{Deref, DerefMut},
+    rc::Rc,
+};
 
 #[derive(Debug, PartialEq)]
 struct List<T> {
     val: T,
-    next: Option<Box<List<T>>>,
+    next: Option<ListNode<T>>,
 }
 
-type ListNode<T> = Box<List<T>>;
+type ListNode<T> = Rc<List<T>>;
 
 impl<T> List<T> {
-    pub fn new(val: T) -> Box<List<T>> {
-        Box::new(List { val, next: None })
+    pub fn new(val: T) -> List<T> {
+        List { val, next: None }
     }
 
     pub fn add_back(&mut self, new_node: ListNode<T>) {
         self.next = Some(new_node);
+    }
+
+    pub fn last(self) -> ListNode<T> {
+        let mut last = Rc::new(self);
+
+        while let Some(next) = last.next.clone() {
+            last = next;
+        }
+
+        last
     }
 }
 
@@ -26,15 +39,10 @@ impl<T> Deref for List<T> {
     }
 }
 
-impl<T> Iterator for List<T> {
-    type Item = ListNode<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next
-    }
-}
 #[cfg(test)]
 mod tests {
+    use std::rc::Rc;
+
     use crate::List;
 
     #[test]
@@ -52,18 +60,19 @@ mod tests {
     #[test]
     fn add_back() {
         let mut head = List::new(1);
-        head.add_back(List::new(2));
+        head.add_back(Rc::new(List::new(2)));
 
         assert_eq!(head.next.unwrap().val, 2);
     }
 
     #[test]
     fn last() {
-        let mut head = List::new(1);
-        head.next = Some(List::new(2));
-        let mut last = head.next.unwrap();
-        last.next = Some(List::new(3));
+        let mut head = Rc::new(List::new(1));
 
-        assert_eq!(head.last().unwrap().val, 3);
+        let mut last = head.next.unwrap();
+
+        last.next = Some(Rc::new(List::new(3)));
+
+        assert_eq!(head.last().val, 3);
     }
 }
